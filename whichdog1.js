9bb7386afa2972b03dd6c62c2071c9c1c8b9072a
Ana Beatriz <anascarpino@gmail.com>
2014-10-03 17:35:18 -0300

    Versão mais atualizada

--- whichdog1.js
+++ whichdog1.js
@@ -155,16 +155,22 @@
 	//função se seleção de abas
 	function sel(idaba){
 		var aba=document.getElementById(idaba);
-		var nAbas="4"; <!-- colocar o nÃºmero de abas  1
-		for(var i="1";i<nAbas;i++){
+		var nAbas=4;
+		for(var i=1;i<=nAbas;i++){
 			var id="aba"+i;
 			document.getElementById(id).className="unsel";
 		}
 		aba.className="sel";
-		for(var u="1";u<nAbas;u++){
+		for(var u=1;u<=nAbas;u++){
 			var idt="textaba"+u;
 			document.getElementById(idt).className="divunsel";
 		}
 		var iddiv="text"+idaba;
 		document.getElementById(iddiv).className="divsel";
 	}
+	
+	sel('aba1');
+	
+	//document.getElementById('t_abas').addEventListener('click', function (e) {
+		//console.log(e);
+	//});
