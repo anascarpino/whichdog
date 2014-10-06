653fe10959539ef7d2150d4f245c0628f56a92cc
Ana Beatriz <anascarpino@gmail.com>
2014-10-06 17:15:56 -0300

    Retirada alguns comandos do arquivo javascript, passados para o arquivo css.

--- whichdog1.js
+++ whichdog1.js
@@ -1,20 +1,5 @@
 window.onload = main;
 function main(){
-	document.getElementById("id1").style.color = "white";
-	document.getElementById("id1").style.fontSize = "120px";
-	document.getElementById("id1").style.fontFamily = "Arial";
-	document.getElementById("img1").style.maxWidth = "700px";
-	document.getElementById("img1").style.maxHeight = "300px";
-	document.getElementById("img1").style.cssFloat = "left";
-	document.getElementById("p1").style.color = "white";
-	document.getElementById("id2").style.textAlign = "center";
-	document.getElementById("aba1").style.width = "20%";
-	document.getElementById("aba2").style.width = "30%";
-	document.getElementById("aba3").style.width = "30%";
-	document.getElementById("aba1").style.height = "40%";
-	document.getElementById("aba2").style.height = "40%";
-	document.getElementById("aba3").style.height = "40%";
-
 	//função se seleção de abas
 	function sel(idaba){
 		var aba=document.getElementById(idaba);
@@ -32,22 +17,9 @@ function main(){
 		document.getElementById(iddiv).className="divsel";
 	}
 	
-	document.getElementById('aba1').addEventListener('click', function(){
-		sel('aba1');
+	document.getElementById('tr_abas').addEventListener('click', function(e){
+		sel(e.target.id);
 	});
-	
-	document.getElementById('aba2').addEventListener('click', function(){
-		sel('aba2');
-	});
-	
-	document.getElementById('aba3').addEventListener('click', function(){
-		sel('aba3');
-	});
-	
-	document.getElementById('aba4').addEventListener('click', function(){
-		sel('aba4');
-	});
-	
 	sel('aba1');
 }
 
